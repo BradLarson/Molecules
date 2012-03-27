caf94cadd9bc3ab79105a54a133b59bcde736db5
Brad Larson <larson@sunsetlakesoftware.com>
2012-03-27 14:47:07 +0000

    Updated project to really disable Thumb for ARMv6 and leave it on for ARMv7.

--- main.m
+++ main.m
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 int main(int argc, char *argv[]) {
-	
+    
 	@autoreleasepool {
 		int retVal = UIApplicationMain(argc, argv, nil, @"SLSMoleculeAppDelegate");
 		return retVal;
